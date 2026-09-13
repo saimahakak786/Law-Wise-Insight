feaf192efe1b83203cf52a8df7365ae98a92d38a
saimahakak786 <advmursaleen123@gmail.com>
2026-09-13 23:40:55 +0530

    Update UpgradeModal.tsx

--- artifacts/mobile/components/UpgradeModal.tsx
+++ artifacts/mobile/components/UpgradeModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Modal, SafeAreaView } from 'expo-router';
+import { View, Text, StyleSheet, TouchableOpacity, Modal, SafeAreaView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import * as Haptics from 'expo-haptics';
 
